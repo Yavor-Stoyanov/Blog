bf638335f2c26f46447a3035bd5c05e12c03173a
Yavor Stoyanov <jstojanov2@gmail.com>
2024-10-17 21:18:00 +0300

    Add fuction for getting weather from API.

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 import express from "express";
+import axios from "axios";
 
 const app = express();
 const PORT = 3000;
+let cachedWeather;
+let lastFetchTime;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res, next) => {
+async function fetchWeather() {
+    const currentTime = new Date().getTime();
+    if (!lastFetchTime || currentTime - lastFetchTime > 15 * 60 * 1000) {
+        const url = 'https://api.openweathermap.org/data/2.5/weather?lat=42.698334&lon=23.319941&units=metric&lang=bg&appid=54e3f85ed731175621163b6963ad1bba';
+        try {
+            const responce = await axios.get(url);
+            cachedWeather = responce.data;
+            lastFetchTime = currentTime;
+        } catch (error) {
+            console.error('Error fetching weather:', error.message);
+            cachedWeather = null;
+        }
+    }
+    return cachedWeather;
+}
+
+app.get('/', async (req, res, next) => {
+    try {
+        const weather = await fetchWeather();
+        res.locals.weather = weather || { error: 'Weather data unavailable' };
+
+    } catch (error) {
+
+    }
+
     try {
         res.render('index.ejs', {
             headerLinks: [
@@ -21,12 +48,12 @@ app.get('/', (req, res, next) => {
 
 app.get('/register', (req, res, next) => {
     try {
-        res.render('register.ejs', { 
+        res.render('register.ejs', {
             headerLinks: [
                 { text: 'Home', url: '/' },
                 { text: 'Login', url: '/login' }
             ]
-         });
+        });
     } catch (error) {
         next(error);
     }
