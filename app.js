8cff5a56afa3cc249c45ec6dbc5e698ddfc76225
Yavor Stoyanov <jstojanov2@gmail.com>
2024-10-18 19:27:36 +0300

    Correct app.js for weather api fetch.

--- app.js
+++ app.js
@@ -10,40 +10,31 @@ app.use(express.static('public'));
 
 async function fetchWeather() {
     const currentTime = new Date().getTime();
-    if (!lastFetchTime || currentTime - lastFetchTime > 15 * 60 * 1000) {
+    if (!lastFetchTime || currentTime - lastFetchTime > 60 * 60 * 1000) {
         const url = 'https://api.openweathermap.org/data/2.5/weather?lat=42.698334&lon=23.319941&units=metric&lang=bg&appid=54e3f85ed731175621163b6963ad1bba';
         try {
-            const responce = await axios.get(url);
-            cachedWeather = responce.data;
+            const response = await axios.get(url);
+            cachedWeather = response.data;
             lastFetchTime = currentTime;
         } catch (error) {
-            console.error('Error fetching weather:', error.message);
+            console.error(error.stack);
             cachedWeather = null;
         }
     }
     return cachedWeather;
 }
 
-app.get('/', async (req, res, next) => {
-    try {
-        const weather = await fetchWeather();
-        res.locals.weather = weather || { error: 'Weather data unavailable' };
+app.get('/', async (req, res) => {
+    const weather = await fetchWeather();
+    res.locals.weather = weather;
 
-    } catch (error) {
-
-    }
-
-    try {
-        res.render('index.ejs', {
-            headerLinks: [
-                { text: 'Login', url: '/login' },
-                { text: 'Register', url: '/register' },
-                { text: 'Contact', url: '/contact' }
-            ]
-        });
-    } catch (error) {
-        next(error);
-    }
+    res.render('index.ejs', {
+        headerLinks: [
+            { text: 'Login', url: '/login' },
+            { text: 'Register', url: '/register' },
+            { text: 'Contact', url: '/contact' }
+        ]
+    });
 });
 
 app.get('/register', (req, res, next) => {
@@ -62,12 +53,14 @@ app.get('/register', (req, res, next) => {
 app.use((err, req, res, next) => {
     console.error(err.stack);
 
-    const statusCode = err.statusCode || 500;
-
-    res.status(statusCode).json({
+    res.status(err.statusCode).json({
         success: false,
-        message: err.message || 'Internal Server Error'
+        message: err.message
     });
+
+    //res.render('error.ejs', {
+    //     message: err.message || 'Internal Server Error'
+    // });
 });
 
 app.listen(PORT, () => {
