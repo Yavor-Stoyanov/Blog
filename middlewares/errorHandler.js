ee1b676f004ba71d84664daf40c6e6162baa39a1
Yavor Stoyanov <jstojanov2@gmail.com>
2024-11-25 17:46:29 +0200

    Add better css dor error page.

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,12 +2,11 @@ export const errorHandler = (err, req, res, next) => {
     console.error('Error message:', err.message);
     console.error('Error stack:', err.stack);
 
-    const statusCode = err.statusCode || 500; // Задава статус код по подразбиране
+    const statusCode = err.statusCode || 500;
 
-    // Рендерира страницата за грешки
     res.status(statusCode).render('error.ejs', {
-        title: 'Error', // Заглавие на страницата
-        message: err.message || 'Something went wrong!', // Съобщение за потребителя
-        statusCode // Статус код за информация
+        title: 'Error',
+        message: err.message || 'Something went wrong!',
+        statusCode
     });
 };
