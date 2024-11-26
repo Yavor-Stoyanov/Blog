402ba9dc34480e03716b6c4f4984405560a3b05d
Yavor Stoyanov <jstojanov2@gmail.com>
2024-11-26 19:28:34 +0200

    Add user friendly message for duplicate titles.

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,12 +1,16 @@
 export const errorHandler = (err, req, res, next) => {
     console.error('Error message:', err.message);
     console.error('Error stack:', err.stack);
-
-    const statusCode = err.statusCode || 500;
+    
+    let message = err.message;
+    let statusCode = err.statusCode || 500;
+    
+    if (err.message.includes('duplicate') && err.message.includes('title')) {
+        message = 'The title of the post already exists. Please, type different one.'
+    }
 
     res.status(statusCode).render('error.ejs', {
-        title: 'Error',
-        message: err.message || 'Something went wrong!',
+        message: message || 'Something went wrong!',
         statusCode
     });
 };
