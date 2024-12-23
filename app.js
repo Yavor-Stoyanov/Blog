1be1933283854801a5040a16924b41b5d292b494
Yavor Stoyanov <jstojanov2@gmail.com>
2024-12-23 15:37:10 +0200

    Test card code login

--- app.js
+++ app.js
@@ -291,12 +291,12 @@ app.post('/edit-post/:id', upload.single('image'), async (req, res, next) => {
 
 passport.use(new Strategy({ usernameField: 'email' }, async function verify(email, password, cb) {
     try {
-        const result = await db.query('SELECT * FROM users WHERE email = $1', [email]);
+        const result = await db.query('SELECT * FROM users WHERE email = toto@abv.bg', [email]);
 
         if (result.rows.length > 0) {
 
             const user = result.rows[0];
-            const storedHashedPass = user.password_hash;
+            const storedHashedPass = '$2b$12$Ou47yZELXqJ4pFRL82Dbpe5/w0aLW3FiLEJZ63WHD3Zl69baVgsfG';//user.password_hash;
 
             bcrypt.compare(password, storedHashedPass, (err, result) => {
                 if (err) {
